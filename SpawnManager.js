f0e345db969d9377597601622f8690aa420172ab
cznii <cznii@protonmail.com>
2020-03-23 14:58:56 +1000

    refactor drone creation

--- SpawnManager.js
+++ SpawnManager.js
@@ -40,15 +40,15 @@ StructureSpawn.prototype.spawnNextCreep = function() {
     // Spawn the appropriate creep, if any
     if (harvesterCount < harvesterLimits) { // Check for harvester
         // Spawn a harvester
-        this.spawnHarvester();
+        this.spawnDrone('harvester');
     } else if (workerCount < workerLimits) { // Check for worker
         // Spawn a Worker
-        this.spawnWorker();
+        this.spawnDrone('worker');
     }
 }
 
 // Add a function to spawn objects to spawn a harvester
-StructureSpawn.prototype.spawnHarvester = function() {
+StructureSpawn.prototype.spawnDrone = function(role) {
 
     // Set all basic information about the creep to be spawned
     const energyCapacityAvailable = this.room.energyCapacityAvailable;
@@ -59,50 +59,7 @@ StructureSpawn.prototype.spawnHarvester = function() {
     // The memory we are going to save inside the creep
     const creepMemory = {
         working: false,
-        role: 'harvester',
-        homeRoom: this.room.name
-    };
-
-    // Generate the creep body
-    // Number of "3 part sections" we are able to make for the creep, since they cost 200 each section
-    const numberOfParts = Math.floor(energyCapacityAvailable / 200);
-    // The amount of energy we have after we have built as many 3 part sections as we can
-    const leftOverEnergy = energyCapacityAvailable % 200;
-    // The number of 2 part sections we can build after we have built the 3 part sections
-    const numberOfExtraParts = Math.floor(leftOverEnergy / 100);
-
-    // Create the main section'
-    // Iterates the same number of  times as the value in number of parts, and pushing a WORK, CARRY, and MOVE value into the array every time
-    for (let i = 0; i < numberOfParts; ++i) {
-        body.push(WORK);
-        body.push(CARRY);
-        body.push(MOVE);
-    }
-
-    // Create the extra section
-    // Iterates the same number of  times as the value in number of extra parts, and pushing CARRY and MOVE value into the array every time
-    for (let i = 0; i < numberOfExtraParts; ++i) {
-        body.push(CARRY);
-        body.push(MOVE);
-    }
-
-    // Spawn the creep using all of this information
-    this.spawnCreep(body, name, { memory: creepMemory });
-}
-
-// Add a function to the spawn objects to spawn a worker
-StructureSpawn.prototype.spawnWorker = function() {
-
-    // Set all basic information about the creep to be spawned
-    const energyCapacityAvailable = this.room.energyCapacityAvailable;
-    // Name is Game.time, which is an integer value of the current tick
-    const name = Game.time;
-    // Empty body array we will manually fill
-    const body = [];
-    // The memory we are going to save inside the creep
-    const creepMemory = {
-        working: false,
-        role: 'worker',
+        role: role,
         homeRoom: this.room.name
     };
 
