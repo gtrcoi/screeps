03604e9260e49137b2b8dddb951c865d3fcacacc
cznii <cznii@protonmail.com>
2020-03-23 14:20:23 +1000

    Waldrip tut 3

--- WorkerManager.js
+++ WorkerManager.js
@@ -1,14 +1,12 @@
 module.exports = {
 
     // Run the role for the Worker creep
-
     runRole: function(creep) {
 
-        const creepCarry = creep.store[RESOURCE_ENERGY];
-        const creepCarryCapacity = creep.store.getCapacity();
+        const creepCarry = creep.carry.energy;
+        const creepCarryCapacity = creep.carryCapacity
 
         // Cases for switching states
-
         if (creep.memory.working && creepCarry === 0) {
             creep.memory.working = false;
         } else if (!creep.memory.working && creepCarry === creepCarryCapacity) {
@@ -16,32 +14,20 @@ module.exports = {
         }
 
         // Run the creep
-
         if (creep.memory.working) {
 
-            // Creep is working, find the closest spawn and transfer energy
-
+            // Creep is working, find closest spawn and transfer energy
             const controller = creep.room.controller;
-
             if (creep.upgradeController(controller) === ERR_NOT_IN_RANGE) {
                 creep.moveTo(controller);
             }
-
         } else {
 
-            // Creep is not working, get energy from the source
-
+            // Creep is not working, get energy from source
             const source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-
             if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(source, {
-                    visualizePathStyle: {
-                        stroke: '#ffff4d',
-                        opacity: 0.7
-                    }
-                });
+                creep.moveTo(source);
             }
-
         }
     }
 }
--- main.js
+++ main.js
@@ -2,30 +2,37 @@ const roles = {
     harvester: require('./HarvesterManager'),
     worker: require('./WorkerManager'),
 }
-const spawnManager = require('./SpawnManager');
+const SpawnManager = require('./SpawnManager');
 
 module.exports.loop = function() {
-    // Main loop.
-    // Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, CARRY, MOVE, MOVE], Game.time, {memory: {working: false, role: 'harvester'}});
-    // Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, CARRY, MOVE, MOVE], Game.time, {memory: {working: false, role: 'worker'}});
+
+    // Run the harvester manager for every creep in the game
     for (const key in Game.creeps) {
+        // Get the current creep for this iteration of the loop
         const creep = Game.creeps[key];
+        // Call runRole on the corresponding creep role as defined in the roles variable at the top of the file
         roles[creep.memory.role].runRole(creep);
     }
 
+    // set the limits for the creeps
     for (const key in Game.rooms) {
+        // Get the current room for this iteration fo the loop
         const room = Game.rooms[key];
-        // Skip if no controller or not owned
+
+        // Skip this room if its not your room or has no controller
         if (!room.controller || !room.controller.my) {
             continue;
         }
-        spawnManager.setSpawnLimits(room);
+
+        // Set the spawn limits for this room
+        SpawnManager.setSpawnLimits(room);
     }
 
-    // Run spawn next for each spawn
+    // Run the spawn next creep for every spawn
     for (const key in Game.spawns) {
+        // Get the current spawn for this iteration of the loop
         const spawn = Game.spawns[key];
-
+        // Call spawn next creep on this spawn object
         spawn.spawnNextCreep();
     }
-};
+}
