052c4fa7c24b03124ab516e4c1b18dd6721d5a1f
cznii <cznii@protonmail.com>
2020-03-29 21:40:25 +1000

    cleaning

--- TowerManager.js
+++ TowerManager.js
@@ -1,11 +1,10 @@
 // create a new function for StructureTower
-StructureTower.prototype.defend =
-    function() {
-        // find closes hostile creep
-        var target = this.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        // if one is found...
-        if (target != undefined) {
-            // ...FIRE!
-            this.attack(target);
-        }
-    };
+StructureTower.prototype.defend = function() {
+    // find closes hostile creep
+    var target = this.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    // if one is found...
+    if (target != undefined) {
+        // ...FIRE!
+        this.attack(target);
+    }
+};
