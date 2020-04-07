9013df01c4673b29056d927c41c4be5ad4639b71
cznii <cznii@protonmail.com>
2020-04-08 04:11:04 +1000

    add neutrals to buildRamparts

--- StructureManager.js
+++ StructureManager.js
@@ -4,7 +4,7 @@ module.exports = {
 
         var rampartBuildings = room.find(FIND_MY_STRUCTURES, {
             filter: (b) =>
-                b.structureType === STRUCTURE_CONTAINER ||
+                b.structureType === STRUCTURE_LINK ||
                 b.structureType === STRUCTURE_EXTENSION ||
                 b.structureType === STRUCTURE_SPAWN ||
                 b.structureType === STRUCTURE_STORAGE ||
@@ -13,7 +13,9 @@ module.exports = {
                 b.structureType === STRUCTURE_TERMINAL ||
                 b.structureType === STRUCTURE_FACTORY
         });
-
+        for (s of room.find(FIND_STRUCTURES, { filter: (s) => s.structureType === STRUCTURE_CONTAINER })) {
+            rampartBuildings.push(s);
+        }
         for (const key in rampartBuildings) {
             let building = rampartBuildings[key];
             if (building.pos.lookFor(LOOK_STRUCTURES, { filter: (b) => b.structureType === STRUCTURE_RAMPART }).length != 0) {
