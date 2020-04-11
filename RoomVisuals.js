c24e1a45f015066b93f7cf10410504b68c06e0fb
cznii <cznii@protonmail.com>
2020-04-11 10:55:28 +1000

    offset paint layout scan

--- RoomVisuals.js
+++ RoomVisuals.js
@@ -2,21 +2,23 @@ module.exports = {
     paintLayoutScan: function(room) {
         const layoutScanPos = room.memory.layoutScan.pos;
 
-        for (let point = 0; point <= 12; point++) {
-            room.visual.line(layoutScanPos.x + point, layoutScanPos.y, layoutScanPos.x + point, layoutScanPos.y + 12, {
+        for (let point = 0; point <= 13; point++) {
+            room.visual.line(layoutScanPos.x + point - 0.5, layoutScanPos.y - 0.5, layoutScanPos.x + point - 0.5, layoutScanPos.y + 13 - 0.5, {
                 opacity: 0.1
             });
-            room.visual.line(layoutScanPos.x, layoutScanPos.y + point, layoutScanPos.x + 12, layoutScanPos.y + point, {
+            room.visual.line(layoutScanPos.x - 0.5, layoutScanPos.y + point - 0.5, layoutScanPos.x + 13 - 0.5, layoutScanPos.y + point - 0.5, {
                 opacity: 0.1
             });
-            room.visual.text(`${point}`, layoutScanPos.x + point, layoutScanPos.y - 1, {
-                font: 0.5,
-                opacity: 0.3
-            })
-            room.visual.text(`${point}`, layoutScanPos.x - 1, layoutScanPos.y + point, {
-                font: 0.5,
-                opacity: 0.3
-            })
+            if (point < 13) {
+                room.visual.text(`${point}`, layoutScanPos.x + point, layoutScanPos.y - 0.8, {
+                    font: 0.5,
+                    opacity: 0.3
+                })
+                room.visual.text(`${point}`, layoutScanPos.x - 1, layoutScanPos.y + point + 0.2, {
+                    font: 0.5,
+                    opacity: 0.3
+                })
+            }
         }
     }
 }
