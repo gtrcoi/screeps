7344eb93ef74ec3675dc01cf37e426d4a88ce852
cznii <cznii@protonmail.com>
2020-04-13 11:50:59 +1000

    clean up variables

--- RoomVisuals.js
+++ RoomVisuals.js
@@ -1,20 +1,22 @@
 module.exports = {
     paintLayoutScan: function(room) {
-        const layoutScanPos = room.memory.layoutScan.pos;
+        const x = room.memory.layoutScan.pos.x;
+        const y = room.memory.layoutScan.pos.y;
 
-        for (let point = 0; point <= 13; point++) {
-            room.visual.line(layoutScanPos.x + point - 0.5, layoutScanPos.y - 0.5, layoutScanPos.x + point - 0.5, layoutScanPos.y + 13 - 0.5, {
+        room.visual.circle(x, y)
+        for (let i = 0; i <= 13; i++) {
+            room.visual.line(x + i - 0.5, y - 0.5, x + i - 0.5, y + 13 - 0.5, {
                 opacity: 0.1
             });
-            room.visual.line(layoutScanPos.x - 0.5, layoutScanPos.y + point - 0.5, layoutScanPos.x + 13 - 0.5, layoutScanPos.y + point - 0.5, {
+            room.visual.line(x - 0.5, y + i - 0.5, x + 13 - 0.5, y + i - 0.5, {
                 opacity: 0.1
             });
-            if (point < 13) {
-                room.visual.text(`${point}`, layoutScanPos.x + point, layoutScanPos.y - 0.8, {
+            if (i < 13) {
+                room.visual.text(`${i}`, x + i, y - 0.8, {
                     font: 0.5,
                     opacity: 0.3
                 })
-                room.visual.text(`${point}`, layoutScanPos.x - 1, layoutScanPos.y + point + 0.2, {
+                room.visual.text(`${i}`, x - 1, y + i + 0.2, {
                     font: 0.5,
                     opacity: 0.3
                 })
